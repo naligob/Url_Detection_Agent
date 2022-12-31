2ef0fe20048c7978bfec380f85cb028ae79dfcfd
PUBLICIS\ilan_b <ilan_b@mintapp.co.il>
2022-12-31 15:51:02 +0200

    Add project files.

---
+++ App.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using UrlTcpListenerLibrary.Services;
+
+namespace Url_Detection_Agent;
+
+public class AgentApp
+{
+	private readonly ITcpService _tcpService;
+	private readonly IProxyServerService _proxyServerService;
+	private readonly ILogger<AgentApp> _log;
+
+	public AgentApp(ITcpService tcpService,ILogger<AgentApp> log,IProxyServerService proxyServerService)
+	{
+		_tcpService = tcpService;
+		_log = log;
+		_proxyServerService = proxyServerService;
+	}
+
+
+	public void Run(string[] args)
+	{
+		_log.LogInformation("Agent Start");
+		//_tcpService.RunTcpListeners();
+		_proxyServerService.RunProxy();
+		
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using UrlTcpListenerLibrary.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Url_Detection_Agent;
+
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+// general run of the app
+try
+{
+    services.GetRequiredService<AgentApp>().Run(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+	throw;
+}
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+        {
+            services.AddSingleton<ITcpService, TcpService>();
+            services.AddSingleton<AgentApp>();
+            services.AddSingleton<IProxyServerService,ProxyServerService>();
+        });
+}
